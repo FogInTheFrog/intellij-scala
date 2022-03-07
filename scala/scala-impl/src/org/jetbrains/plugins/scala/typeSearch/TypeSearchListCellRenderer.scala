ef25bd91285634d7fe5239e495c9f697d7aa8656
Jakub Zacharczuk <zacharczuk.jakub2k@gmail.com>
2022-03-08 00:49:33 +0100

    refactor code

--- scala/scala-impl/src/org/jetbrains/plugins/scala/typeSearch/TypeSearchListCellRenderer.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/typeSearch/TypeSearchListCellRenderer.scala
@@ -9,83 +9,64 @@ import org.jetbrains.plugins.scala.lang.psi.types.TypePresentationContext
 
 class TypeSearchListCellRenderer() extends PsiElementListCellRenderer[PsiMethod] {
 
+  def wrapInBrackets(s: String): String = {
+    "(" + s + ")"
+  }
+
+  def getScalaParameters(element: PsiMethod): String = {
+    element
+      .asInstanceOf[ScFunctionWrapper]
+      .delegate
+      .parameters
+      .map(x => x.typeElement.get.`type`().getOrNothing)
+      .mkString(", ")
+  }
+
+  def getScalaReturnType(element: PsiMethod): String = {
+    element.asInstanceOf[ScFunctionWrapper]
+      .delegate
+      .returnType
+      .get
+      .presentableText(TypePresentationContext.emptyContext)
+  }
+
   def renderMethodTypes(element: PsiMethod): String = {
     try {
-      "(".concat(
-        element
-          .asInstanceOf[ScFunctionWrapper]
-          .delegate
-          .parameters
-          .map(
-            x => x.typeElement.get.`type`().getOrNothing
-          )
-          .mkString(", "))
-        .concat(")")
+      wrapInBrackets(getScalaParameters(element))
         .concat(" => ")
-        .concat(
-          element.asInstanceOf[ScFunctionWrapper]
-            .delegate
-            .returnType
-            .get
-            .presentableText(TypePresentationContext.emptyContext)
-        )
+        .concat(getScalaReturnType(element))
     }
     catch {
-      case _: Throwable => "(" + element.getContainingClass.getName + " => ".concat(element
-        .getHierarchicalMethodSignature
-        .getParameterTypes.map(x => x.getPresentableText + " => ").mkString.concat(element.getReturnType
-        .getPresentableText)) + ")"
+      case _: Throwable =>
+        wrapInBrackets(
+          element
+            .getHierarchicalMethodSignature
+            .getParameterTypes
+            .map(x => x.getPresentableText)
+            .mkString(", ")
+        )
+          .concat(" => ")
+          .concat(
+            element
+              .getReturnType
+              .getPresentableText
+          )
     }
   }
 
   override def getElementText(element: PsiMethod): String = element.getName + renderMethodTypes(element)
 
   override def getContainerText(element: PsiMethod, name: String): String = {
-    // element.asInstanceOf[ScFunctionWrapper]
-    // element.asInstanceOf[ScFunctionWrapper].delegate.returnType.get.presentableText(TypePresentationContext.emptyContext)
-    // element.asInstanceOf[ScFunctionWrapper].delegate.parameters.map(x => x.typeElement.toString)
-    // element.asInstanceOf[ScFunctionWrapper].delegate.parameters.map(x => x.typeElement.get.`type`())
     try {
-      element.asInstanceOf[ScFunctionWrapper].delegate.getContainingClass.asInstanceOf[ScClassImpl].getPath
+      element
+        .asInstanceOf[ScFunctionWrapper]
+        .delegate
+        .getContainingClass
+        .asInstanceOf[ScClassImpl]
+        .getPath
     }
     catch {
       case _: Throwable => element.getContainingClass.getName
     }
-    // element.asInstanceOf[ScFunctionWrapper].delegate.parameters.map(x => x.typeElement.get.`type`().get.toString)
-
-//    renderMethodTypes(element)
-
-
-    //      element.asInstanceOf[ScFunctionWrapper].delegate.parameters.map(x => x.typeElement.get.`type`().getOrNothing)
-    //        .mkString(", ") +
-    //      " => " +
-    //      element.asInstanceOf[ScFunctionWrapper].delegate.returnType.get.presentableText(TypePresentationContext.emptyContext)
-
-    //    element
-    //      .getSignature(PsiSubstitutor.EMPTY)
-    //      .getParameterTypes
-    //      .map(x => x.getPresentableText + " => ")
-    //      .mkString
-    //      .concat(
-    //        element
-    //          .getReturnType
-    //          .getPresentableText
-    //      )
-
-//        element
-//          .getContainingClass
-//          .getName + " => "
-//          .concat(
-//            element
-//              .getHierarchicalMethodSignature
-//              .getParameterTypes
-//              .map(x => x.getPresentableText + " => ")
-//              .mkString
-//              .concat(
-//                element
-//                  .getReturnType
-//                  .getPresentableText
-//              )
-//          )
   }
 }
